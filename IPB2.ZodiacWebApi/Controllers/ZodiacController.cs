3c2293ed83f138e887eed3f56325fffcf9107daa
EmiriMaung <poelay1995@gmail.com>
2026-03-04 20:59:09 +0630

    feat: ZodiacController

---
+++ IPB2.ZodiacWebApi/Controllers/ZodiacController.cs
@@ -0,0 +1,28 @@
+﻿using IPB2.ZodiacWebApi.Database.AppDbContextModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPB2.ZodiacWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZodiacController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ZodiacController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _context.ZodiacSigns
+                                     .Include(x => x.Traits)
+                                     .ToListAsync();
+
+            return Ok(data);
+        }
+    }
+}
--- IPB2.ZodiacWebApi/Program.cs
+++ IPB2.ZodiacWebApi/Program.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,16 +14,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
